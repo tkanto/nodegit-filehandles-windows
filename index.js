9188f92a693d62e26f69c38dd89f44dfe42ffa09
Teemu Kantoluoto <teemu.kantoluoto@ray.fi>
2016-10-05 14:14:40 +0300

    RevWalk is the real culprit!
    After discovering that repository.cleanup() closes filehandles after
    simple operations like getHeadCommit(), but my production code still
    failing to work, I discovered that it would clear up if I never used
    RevWalk. Unfortunately RevWalk is handy for the implementation I'm using
    and I'd like to keep using it, I made this alteration to this repository,
    which reproduces the problem.

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ var cloneOpts = {
 };
 
 var repository_url = "https://github.com/tkanto/nodegit-filehandles-windows.git"
+var repo = null
+var headCommit = null
 
 //Cleanup any leftovers in the test directory
 fs.remove("./test_repository", function(err) {
@@ -20,15 +22,38 @@ fs.remove("./test_repository", function(err) {
   .then(function() {
       return git.Repository.open("./test_repository")
   })
-  .then(function(repo) {
+  .then(function(repository) {
     console.log("Repository cloned. If we tried to remove the directory here, it would succesfully remove the directory.");
-    return repo.getHeadCommit()
+    repo = repository
+    return repo.getBranchCommit("master")
   })
+
   .then(function(head) {
+    headCommit = head
+    return repo.getBranchCommit("origin/master")
+  })
+  .then(function(upstreamCommit) {
+    var walker = repo.createRevWalk()
+    walker.sorting(git.Revwalk.SORT.Time)
+    walker.push(headCommit.sha())
+    return walker.getCommitsUntil(function (commit) {
+      return commit === upstreamCommit.sha()
+    })
+  })
+  .then(function (array) {
+    console.log("Revwalk produced: " + array)
+    return repo.getHeadCommit() //just so we return a promise
+  })
+
+  .then(function () {
     console.log("But now that we've accessed data from the repository, the .git/objects/pack/* files are opened but not closed, and the program will hang when trying to remove the directory.")
+    repo.cleanup()
     fs.remove("./test_repository", function (err) {
       if (err) { console.error(err); process.exit(1) }
       console.log("./test_repository succesfully removed. This will not occur on a windows machine.")
     });
+  })
+  .catch(function (err) {
+    console.error(err)
   })
 });
